d38489d963a0f684f90dfc7f300dbf324bbb75de
Pablo Sousa <pablosousa.ads@gmail.com>
2018-12-05 23:43:18 -0200

    added CRUD Test and Routes to Movies

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import config from './config/config';
 import datasource from './config/datasource';
 
@@ -6,6 +7,7 @@ const app = express();
 app.config = config;
 app.datasource = datasource(app);
 app.set('port', 9000);
+app.use(bodyParser.json());
 
 // load model Movies
 const Movies = app.datasource.models.Movies
@@ -18,6 +20,35 @@ app.route('/movies')
     Movies
       .findAll({})
         .then(result => res.json(result))
+        .catch(err => res.status(412));
+  })
+  .post((req, res) => {
+    Movies
+      .create(req.body)
+        .then(result => res.json(result))
+        .catch(err => res.status(412));
+  });
+
+// route movie
+app.route('/movies/:id')
+  .get((req, res) => {
+    
+    // find all movies in database
+    Movies
+      .findOne({where: req.params})
+        .then(result => res.json(result))
+        .catch(err => res.status(412));
+  })
+  .put((req, res) => {
+    Movies
+      .update(req.body, {where: req.params})
+        .then(result => res.json(result))
+        .catch(err => res.status(412));
+  })
+  .delete((req, res) => {
+    Movies
+      .destroy({where: req.params})
+        .then(result => res.sendStatus(204))
         .catch(err => res.status(412));
   });
 
--- test/integration/app.js
+++ test/integration/app.js
@@ -38,4 +38,74 @@ describe('Routes Movies', () => {
         });
     });
   });
+
+  describe('Route GET /movies/{id}', () => {
+    it('should return a movie', done => {
+  
+      request
+        .get('/movies/1')
+        .end((err, res) => {
+          
+          expect(res.body.id).to.be.eql(defaultMovie.id);
+          expect(res.body.name).to.be.eql(defaultMovie.name);
+          
+          done(err);
+        });
+    });
+  });
+
+  describe('Route POST /movies', () => {
+    it('should create a movie', done => {
+  
+      const newMovie = {
+        id: 2,
+        name: 'Lord of Rings'
+      };
+
+      request
+        .post('/movies')
+        .send(newMovie)
+        .end((err, res) => {
+          
+          expect(res.body.id).to.be.eql(newMovie.id);
+          expect(res.body.name).to.be.eql(newMovie.name);
+          
+          done(err);
+        });
+    });
+  });
+
+  describe('Route PUT /movies/{id}', () => {
+    it('should update a movie', done => {
+  
+      const updatedMovie = {
+        id: 1,
+        name: 'Harry Potter'
+      };
+
+      request
+        .put('/movies/1')
+        .send(updatedMovie)
+        .end((err, res) => {
+          
+          expect(res.body).to.be.eql([1]);
+          
+          done(err);
+        });
+    });
+  });
+
+  describe('Route DELETE /movies/{id}', () => {
+    it('should delete a movie', done => {
+
+      request
+        .delete('/movies/1')
+        .end((err, res) => {
+
+          expect(res.statusCode).to.be.eql(204);
+          
+          done(err);
+        });
+    });
+  });
 });
