87d012a997d33849350ff12c4dcfc12d37a410a4
Pablo Sousa <pablosousa.ads@gmail.com>
2018-12-09 15:07:45 -0200

    create database test

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').load();
 
 export default {
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database: process.env.NODE_ENV === 'test' ? process.env.TEST_DB_DATABASE : process.env.DB_DATABASE,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   params: {
